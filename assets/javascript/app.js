41ddd8c84f1508bbeda648fea99b033c8cd57faf
Dylan Thomas <thomasdylan76@gmail.com>
2019-09-20 11:59:19 -0600

    Pulling from API and updating DOM completed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,20 @@
-// api key: bB8FrIVzuWKq1ovd36ElbBSdYruDc9FJ
+var search = "tool band";
+var apiKey = "bB8FrIVzuWKq1ovd36ElbBSdYruDc9FJ";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&limit=10&rating=pg-13&apikey=" + apiKey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    for (var i = 0; i < response.data.length; i++) {
+        var row = $("<div>");
+        var gif = $("<img>");
+        var rating = $("<p>");
+        gif.attr("src", response.data[i].images.fixed_height.url);
+        rating.append(response.data[i].rating);
+        row.append(gif);
+        row.append(rating);
+        $("#gifs").prepend(row);
+    }
+    console.log(response)
+});
