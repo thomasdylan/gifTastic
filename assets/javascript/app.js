75c60ea86c0ce9277d3f51a79e9154a792bf4b3d
Dylan Thomas <thomasdylan76@gmail.com>
2019-09-21 13:29:00 -0600

    Cleaned up call to API.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,29 @@
-var search = "tool band";
+var topics = ["csgo", "apex legends", "borderlands 3"];
 var apiKey = "bB8FrIVzuWKq1ovd36ElbBSdYruDc9FJ";
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&limit=10&rating=pg-13&apikey=" + apiKey;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    for (var i = 0; i < response.data.length; i++) {
-        var row = $("<div>");
-        var gif = $("<img>");
-        var rating = $("<p>");
-        gif.attr("src", response.data[i].images.fixed_height.url);
-        rating.append(response.data[i].rating);
-        row.append(gif);
-        row.append(rating);
-        $("#gifs").prepend(row);
-    }
-    console.log(response)
-});
+for (var i = 0; i < topics.length; i++) {
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&limit=1&rating=pg-13&apikey=" + apiKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // shorten api response
+        var results = response.data;
+
+        // Looping through each index of response.data
+        for (var j = 0; j < results.length; j++) {
+            var gifDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[j].rating);
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[j].images.fixed_height.url);
+            gifDiv.append(gifImage);
+            gifDiv.append(p);
+
+            $(".gifs").prepend(gifDiv);
+        }
+        
+    });
+}
