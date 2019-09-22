1184467b1ac08494f5eb1888df08848ebfd30161
Dylan Thomas <thomasdylan76@gmail.com>
2019-09-22 12:34:57 -0600

    Last commit before restarting out of frustration.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var apiKey = "bB8FrIVzuWKq1ovd36ElbBSdYruDc9FJ";
 
 for (var i = 0; i < topics.length; i++) {
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&limit=1&rating=pg-13&apikey=" + apiKey;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&limit=2&rating=pg-13&apikey=" + apiKey;
 
     $.ajax({
         url: queryURL,
@@ -18,12 +18,27 @@ for (var i = 0; i < topics.length; i++) {
             var gifDiv = $("<div>");
             var p = $("<p>").text("Rating: " + results[j].rating);
             var gifImage = $("<img>");
-            gifImage.attr("src", results[j].images.fixed_height.url);
-            gifDiv.append(gifImage);
+            gifImage.attr("src", results[j].images.fixed_height_still.url);
+            gifImage.attr("data-still", results[j].images.fixed_height_still.url);
+            gifImage.attr("data-state", "still");
+            gifImage.attr("data-animate", results[j].images.fixed_height.url);
+            gifImage.addClass("gifs");
             gifDiv.append(p);
-
-            $(".gifs").prepend(gifDiv);
+            gifDiv.append(gifImage);
+            $(".gifHolder").prepend(gifDiv);
         }
         
     });
 }
+
+$(".gifs").on("click", function () {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
