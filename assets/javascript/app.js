8cf6cff6cd93b32827698737e8fcb2cd64737a2c
Dylan Thomas <thomasdylan76@gmail.com>
2019-09-22 18:29:30 -0600

    Completed with issue starting and stopping gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var topics = ["csgo", "apex legends", "borderlands 3"];
+    var topics = ["CS:GO", "Fallout 3", "Borderlands 3"];
     var apiKey = "bB8FrIVzuWKq1ovd36ElbBSdYruDc9FJ";
 
     //This stops the enter key from refreshing the page and clearing the buttons.
@@ -11,10 +11,10 @@ $(document).ready(function () {
         }
     });
 
-
+    //Gets 10 gifs from the API and adds them to the DOM with their ratings.
     function showTopicGifs() {
         var search = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&limit=3&rating=pg-13&apikey=" + apiKey;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&limit=10&rating=pg-13&apikey=" + apiKey;
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -27,6 +27,7 @@ $(document).ready(function () {
                 var p = $("<p>").text("Rating: " + results[i].rating);
                 p.css("text-align", "center");
                 var gifImage = $("<img>");
+                gifImage.addClass("indGif");
                 gifImage.attr("src", results[i].images.fixed_height_still.url);
                 gifImage.attr("data-state", "still");
                 gifImage.attr("data-still", results[i].images.fixed_height_still.url);
@@ -38,9 +39,10 @@ $(document).ready(function () {
                 gifDiv.append(gifImage);
                 $(".gifHolder").prepend(gifDiv);
             }
-        })
+        });
     }
 
+    //Loads all the topics as buttons at the top of the DOM.
     function showButtons() {
         for (var i = 0; i < topics.length; i++) {
             var topicButton = $("<button>");
@@ -52,6 +54,24 @@ $(document).ready(function () {
         }
     };
 
+    //Switches the gif between still and playing when clicked.
+    function stillPlay() {
+        $("img").on("click", function () {
+            console.log("Success: " + $(this).attr("data-state"));
+
+            var state = $(this).attr("data-state");
+
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
+    }
+
+    //When clicking the add button a button with the field value is created.
     $("#add-topic").on("click", function (e) {
         e.preventDefault();
         var newTopic = $("#input").val().trim();
@@ -62,50 +82,27 @@ $(document).ready(function () {
         $("#button-holder").empty();
         $("#input").text("");
         showButtons();
-    })
+    });
 
-    function apiLoad() {
+    
+    // $(".gifDiv").on("click", function (e) {
+    //     e.preventDefault();
+        
+    //     console.log("Success: " + $(this).attr("data-state"));
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            // shorten api response
-            var results = response.data;
-            // Looping through each index of response.data
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>");
-                var p = $("<p>").text("Rating: " + results[i].rating);
-                p.css("text-align", "center");
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height_still.url);
-                gifImage.attr("data-state", "still");
-                gifImage.attr("data-still", results[i].images.fixed_height_still.url);
-                gifImage.attr("data-animate", results[i].images.fixed_height.url);
-                gifDiv.addClass("gifs")
-                gifDiv.css("display", "inline-block");
-                gifDiv.css("padding", "5px");
-                gifDiv.append(p);
-                gifDiv.append(gifImage);
-                $(".gifHolder").prepend(gifDiv);
-            }
+    //     var state = $(this).attr("data-state");
 
-            $("img").on("click", function () {
-                console.log("Success: " + $(this).attr("data-state"));
+    //     if (state === "still") {
+    //         $(this).attr("src", $(this).attr("data-animate"));
+    //         $(this).attr("data-state", "animate");
+    //     } else {
+    //         $(this).attr("src", $(this).attr("data-still"));
+    //         $(this).attr("data-state", "still");
+    //     }
+    // });
 
-                var state = $(this).attr("data-state");
-
-                if (state === "still") {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                } else {
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still");
-                }
-            });
-        });
-    }
+    $(document).on("click", ".topic-button", showTopicGifs);
+    $(document).on("click", ".indGif", stillPlay);
 
     showButtons();
 
